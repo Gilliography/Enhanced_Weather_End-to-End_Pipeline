41c97926d21d94a73095fdc404e9db2e6e3329ec
Gilbert Kiprotich <gkiprotich74@gmail.com>
2026-05-16 13:20:40 +0000

    DAG Created

--- dags/weather_streaming_dag.py
+++ dags/weather_streaming_dag.py
@@ -13,3 +13,14 @@ def run_producer():
         ['python', "/opt/airflow/dags/producer.py"]
         check=True
     )
+
+with DAG(
+    dag_id='weather_streaming_dag',
+    default_args=default_args,
+    schedule_interval='*/5 * * * *',
+    catchup=False
+) as dag:
+    producer_task = PythonOperator(
+        task_id=send_weather_dag_to_kafka,
+        python_callable=run_producer
+    )
