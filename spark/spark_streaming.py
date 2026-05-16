5def0d7e603f3c09049bd9cbc26ce83e1ad02a44
Gilbert Kiprotich <gkiprotich74@gmail.com>
2026-05-16 15:46:22 +0000

    spark streaming

---
+++ spark/spark_streaming.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+spark = SparkSession.builder \
+    .appName("WeatherStreaming") \
+    .getOrCreate()
+
+schema = StructType([
+    StructField("time", StringType()),
+    StructField("temperature_2m", DoubleType()),
+    StructField("relative_humidity_2m", DoubleType()),
+    StructField("pressure_msl", DoubleType())
+])
+
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "kafka:9092") \
+    .option("subscribe", "weather-events") \
+    .option("startingOffsets", "latest") \
+    .load()
+
+weather_df = df.selectExpr("CAST(value AS STRING)") \
+
+query = weather_df.writestream \
+    .outputMode("append") \
+    .format("console") \
+    .start()
+
+query.awaitTermination()
